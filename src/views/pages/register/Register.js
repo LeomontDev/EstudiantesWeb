4a91feaaf09249a5cf3bd14de75bf35da1be496b
Luis Leon - Leomont <leomont.co@gmail.com>
2023-07-20 08:41:18 -0400

    registro completado

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   CButton,
   CCard,
@@ -13,8 +13,68 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked } from '@coreui/icons'
+//import { useAuth } from 'src/hooks/useAuth'
+import Swal from 'sweetalert2'
 
 const Register = () => {
+  const [email, setEmail] = useState('')
+  const [pwd, setPwd] = useState('')
+  const [pwd2, setPwd2] = useState('')
+
+  //const { register } = useAuth()
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      )
+  }
+
+  const validatePasswordChars = (pass) => {
+    return String(pass)
+      .toLowerCase()
+      .match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)
+  }
+
+  const handleRegister = async () => {
+    if (email === '' || pwd === '' || pwd2 === '') {
+      Swal.fire({
+        text: 'Debe llenar todos los campos',
+        icon: 'error',
+      })
+      return
+    }
+
+    if (pwd !== pwd2) {
+      Swal.fire({
+        text: 'Las claves no coinciden',
+        icon: 'error',
+      })
+      return
+    }
+
+    if (!validateEmail(email)) {
+      Swal.fire({
+        text: 'Debe ingresar un correo electronico',
+        icon: 'error',
+      })
+      return
+    }
+
+    if (!validatePasswordChars(pwd)) {
+      Swal.fire({
+        text: 'Debe ingresar una contraseña de 6 a 16 caracteres, con al menos 1 letra y 1 numero',
+        icon: 'error',
+      })
+      return
+    }
+
+    console.log({ email, pwd, pwd2 })
+    /* if (continuar)
+      register({ email, password: clave }) */
+  }
+
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -27,7 +87,14 @@ const Register = () => {
                   <p className="text-medium-emphasis">Crea tu cuenta</p>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>@</CInputGroupText>
-                    <CFormInput placeholder="Email" autoComplete="email" />
+                    <CFormInput
+                      placeholder="Email"
+                      autoComplete="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                      type="email"
+                    />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
@@ -37,6 +104,8 @@ const Register = () => {
                       type="password"
                       placeholder="Contraseña"
                       autoComplete="new-password"
+                      value={pwd}
+                      onChange={(e) => setPwd(e.target.value)}
                     />
                   </CInputGroup>
                   <CInputGroup className="mb-4">
@@ -47,12 +116,16 @@ const Register = () => {
                       type="password"
                       placeholder="Repetir contraseña"
                       autoComplete="new-password"
+                      value={pwd2}
+                      onChange={(e) => setPwd2(e.target.value)}
                     />
                   </CInputGroup>
                   <div className="d-grid">
-                    <CButton color="success">Crear cuenta</CButton>
+                    <CButton onClick={handleRegister} color="success">
+                      Crear cuenta
+                    </CButton>
                   </div>
-                  <br />
+                  <hr />
                   <div className="d-grid">
                     <CButton
                       color="primary"
